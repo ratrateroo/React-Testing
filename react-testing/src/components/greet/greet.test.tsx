86217e0a8a126cbb3055c7c189775b9d52481ca4
Mark_Leo <ratrateroo@gmail.com>
2022-08-23 09:50:40 +0800

    tried out test driven development

---
+++ react-testing/src/components/greet/greet.test.tsx
@@ -0,0 +1,17 @@
+//Greet should render the text hello and if a name is passed into the component, it should render hello followed by the name
+
+import { render, screen } from "@testing-library/react";
+import { Greet } from "./greet";
+
+test("Greet renders correctly", () => {
+  render(<Greet />);
+  //const textElement = screen.getByText(/hello/i);
+  const textElement = screen.getByText("Hello");
+  expect(textElement).toBeInTheDocument();
+});
+
+test("Greet renders with name", () => {
+  render(<Greet name="Kimmie" />);
+  const textElement = screen.getByText("Hello Kimmie");
+  expect(textElement).toBeInTheDocument();
+});
