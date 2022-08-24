fccdcde07342bd94055c943098335f84ca568394
Mark_Leo <ratrateroo@gmail.com>
2022-08-24 18:07:36 +0800

    added grouping multiple test using describe

--- react-testing/src/components/greet/greet.test.tsx
+++ react-testing/src/components/greet/greet.test.tsx
@@ -3,16 +3,18 @@
 import { render, screen } from "@testing-library/react";
 import { Greet } from "./greet";
 
-test("Greet renders correctly", () => {
-  render(<Greet />);
-  //const textElement = screen.getByText(/hello/i);
-  const textElement = screen.getByText("Hello");
-  expect(textElement).toBeInTheDocument();
-});
+describe("Greet", () => {
+  test("Greet renders correctly", () => {
+    render(<Greet />);
+    //const textElement = screen.getByText(/hello/i);
+    const textElement = screen.getByText("Hello");
+    expect(textElement).toBeInTheDocument();
+  });
 
-//test.skip() can also be used
-test.only("Greet renders with name", () => {
-  render(<Greet name="Kimmie" />);
-  const textElement = screen.getByText("Hello Kimmie");
-  expect(textElement).toBeInTheDocument();
+  //test.skip() can also be used
+  test.only("Greet renders with name", () => {
+    render(<Greet name="Kimmie" />);
+    const textElement = screen.getByText("Hello Kimmie");
+    expect(textElement).toBeInTheDocument();
+  });
 });
