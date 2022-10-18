6db2945714dddc4c907a360c023a17693f08f751
Mark_Leo <ratrateroo@gmail.com>
2022-10-18 22:52:45 +0800

    added use fetch reducer

--- use-reducer-sample/src/use-fetch-reducer.ts
+++ use-reducer-sample/src/use-fetch-reducer.ts
@@ -1,29 +1,39 @@
 import { Action } from "./action.types";
 import { State } from "./state.types";
 
-function fetchReducer<DataT, ErrorT = string>(
-  currentState: State<DataT, ErrorT>,
-  action: Action<DataT, ErrorT>
-): State<DataT, ErrorT> {
-  switch (action.type) {
-    case "FETCH":
-      return {
-        ...currentState,
-        status: "loading",
-      };
-    case "RESOLVE":
-      return {
-        status: "success",
-        data: action.data,
-        error: null,
-      };
-    case "REJECT":
-      return {
-        data: null,
-        status: "failure",
-        error: action.error,
-      };
-    default:
-      return currentState;
+export function useFetchReducer(initialData = null) {
+  const initialState = {
+    status: "idle",
+    data: initialData,
+    error: null,
+  };
+
+  function fetchReducer<DataT, ErrorT = string>(
+    currentState: State<DataT, ErrorT>,
+    action: Action<DataT, ErrorT>
+  ): State<DataT, ErrorT> {
+    switch (action.type) {
+      case "FETCH":
+        return {
+          ...currentState,
+          status: "loading",
+        };
+      case "RESOLVE":
+        return {
+          status: "success",
+          data: action.data,
+          error: null,
+        };
+      case "REJECT":
+        return {
+          data: null,
+          status: "failure",
+          error: action.error,
+        };
+      default:
+        return currentState;
+    }
   }
+
+  return React.useReducer(fetchReducer, initialState);
 }
