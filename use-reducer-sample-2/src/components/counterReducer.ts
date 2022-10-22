7b34454019c3bd590d50149fcf69d333d25b01dc
Mark_Leo <ratrateroo@gmail.com>
2022-10-22 22:39:48 +0800

    added counter reducer with types

---
+++ use-reducer-sample-2/src/components/counterReducer.ts
@@ -0,0 +1,35 @@
+// An enum with all the types of actions to use in our reducer
+export enum CountActionKind {
+  INCREASE = "INCREASE",
+  DECREASE = "DECREASE",
+}
+
+// An interface for our actions
+interface CountAction {
+  type: CountActionKind;
+  payload: number;
+}
+
+// An interface for our state
+interface CountState {
+  count: number;
+}
+
+// Our reducer function that uses a switch statement to handle our actions
+export function counterReducer(state: CountState, action: CountAction) {
+  const { type, payload } = action;
+  switch (type) {
+    case CountActionKind.INCREASE:
+      return {
+        ...state,
+        count: state.count + payload,
+      };
+    case CountActionKind.DECREASE:
+      return {
+        ...state,
+        count: state.count - payload,
+      };
+    default:
+      return state;
+  }
+}
